21a2ad86b34d5cb2cd78bdacacae569da2a852cf
Lukasz Rozek <lukasz.rozek84@gmail.com>
2013-12-03 17:56:20 +0100

    same in xml

--- src/main/java/pl/lrozek/spring/aop/aspect/AuditAspect.java
+++ src/main/java/pl/lrozek/spring/aop/aspect/AuditAspect.java
@@ -1,17 +1,13 @@
 package pl.lrozek.spring.aop.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-@Aspect
 @Component
 public class AuditAspect {
 
-    @Around("@within(auditable)")
     public Object advice( ProceedingJoinPoint joinPoint, Auditable auditable ) throws Throwable {
         logger.info( "intercepting {} with annotation value '{}'", joinPoint.getSignature().toShortString(), auditable.value() );
         return joinPoint.proceed();
